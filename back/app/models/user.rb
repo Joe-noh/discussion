2218ef6512a94db8b136086b842f8a5908b5a06a
Joe-noh <goflb.jh@gmail.com>
2014-11-06 10:41:01 +0900

    override devise session controller

--- back/app/models/user.rb
+++ back/app/models/user.rb
@@ -1,3 +1,18 @@
 class User < ActiveRecord::Base
+  before_save :ensure_auth_token
+
   devise :database_authenticatable, :registerable
+
+  def ensure_auth_token
+    self.authentication_token = generate_auth_token if authentication_token.blank?
+  end
+
+  private
+
+  def generate_auth_token
+    begin
+      token = Devise.friendly_token
+    end while User.where(authentication_token: token).first
+    token
+  end
 end
